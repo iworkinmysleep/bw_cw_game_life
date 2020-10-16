76900ad9ee74a69cd01d7a71f0b6de4231f9649c
Jason Hinton <jason_hinton@att.net>
2020-10-16 14:55:15 -0400

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conways <span>Game of Life</span></h1>
+    </div>
+  );
+}
+
+export default App;
